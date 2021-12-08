d479f413da80a1ba79ea8fbb48bd8e6b39354ff8
mdb <dauren.asir@gmail.com>
2021-12-08 17:26:11 +0600

    some changes

--- public/logger.js
+++ public/logger.js
@@ -1,97 +1,87 @@
 (function () {
-  /*
-    {
-      url: 'http://limelog/api',
-      tag: 'my-app-name',
+  var ctx = window.LimeLog || {}
+
+  ctx.getXHR = function () {
+    if (typeof XMLHttpRequest !== 'undefined') {
+      return new XMLHttpRequest()
     }
-   */
-
-  let opt = window.LimeLogConfig || {}
-
-  let ctx = {
-    originConsole: {
-      warn: console.warn,
-      error: console.error,
-    },
-
-    opt: opt || {},
-    getXHR () {
-      if (typeof XMLHttpRequest !== 'undefined') {
-        return new XMLHttpRequest()
-      }
+    try {
+      return new ActiveXObject('Msxml2.XMLHTTP')
+    } catch (e) {
       try {
-        return new ActiveXObject('Msxml2.XMLHTTP')
-      } catch (e) {
-        try {
-          return new ActiveXObject('Microsoft.XMLHTTP')
-        } catch (ee) {
+        return new ActiveXObject('Microsoft.XMLHTTP')
+      } catch (ee) {
+      }
+    }
+    return null
+  }
+
+  ctx.fetch = function (url, opts) {
+    var xhr = ctx.getXHR()
+    return (new Promise(function (resolve, reject) {
+      xhr.open(opts.method || 'GET', url, true)
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) return
+        if (xhr.status >= 200 && xhr.status < 300) { // success
+          resolve({ status: xhr.status, text: xhr.responseText })
+        } else {
+          reject({ status: xhr.status, text: xhr.responseText })
         }
       }
-      return null
-    },
-    fetch (url, opts) {
-      let xhr = this.getXHR()
-      return (new Promise(function (resolve, reject) {
-        xhr.open(opts.method || 'GET', url, true)
-        xhr.onreadystatechange = (() => {
-          if (xhr.readyState !== 4) return
-          if (xhr.status >= 200 && xhr.status < 300) { // success
-            resolve({ status: xhr.status, text: xhr.responseText })
-          } else {
-            reject({ status: xhr.status, text: xhr.responseText })
-          }
-        })
-        if (opts.headers) {
-          for (let k in opts.headers) {
-            xhr.setRequestHeader(k, opts.headers[k])
-          }
+      if (opts.headers) {
+        for (var k in opts.headers) {
+          xhr.setRequestHeader(k, opts.headers[k])
         }
-        xhr.send(opts.body || undefined)
-      }))
-    },
-    sendLog (level, body) {
-      body.sf_tag = opt.tag || null
-      body.level = level
-      this.fetch(opt.url, {
-        method: 'POST',
-        body: JSON.stringify([body]),
+      }
+      xhr.send(opts.body || undefined)
+    }))
+  }
+
+  ctx.buffer = ctx.buffer || []
+
+  var originHandler = ctx.handler
+
+  ctx.handler = function () {
+    if (originHandler) {
+      originHandler.apply(ctx, arguments)
+    }
+    ctx.trigger()
+  }
+
+  ctx.sending = false
+  ctx.need_resend = false
+
+  ctx.trigger = function () {
+    if (ctx.sending) {
+      ctx.need_resend = true
+      return
+    }
+    ctx.sending = true
+    var data = []
+    ctx.buffer.forEach(function (item) {
+      var argsArr = Array.prototype.slice.call(item.args)
+      var d = {
+        sf_tag: ctx.tag || null,
+        level: item.level,
+        message: argsArr.join(' '),
+      }
+      argsArr.forEach(function (x, i) {
+        if (x.stack) {
+          d['stacktrace' + (i || '')] = x.stack
+        }
       })
-    },
-  }
-
-  console.error = function () {
-    let args = Array.prototype.slice.call(arguments)
-    let data = {
-      message: args.join(' '),
-    }
-    args.forEach((x, i) => {
-      if (x.stack) {
-        data['stacktrace' + (i || '')] = x.stack
+      data.push(d)
+    })
+    ctx.buffer = []
+    ctx.fetch(ctx.url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).catch(function () {}).then(function () {
+      ctx.sending = false
+      if (ctx.need_resend) {
+        ctx.need_resend = false
+        ctx.trigger()
       }
     })
-    ctx.sendLog('error', data)
-    ctx.originConsole.error.apply(console, arguments)
-  }
-
-  console.warn = function () {
-    let args = Array.prototype.slice.call(arguments)
-    ctx.sendLog('warn', {
-      message: args.join(' '),
-    })
-    ctx.originConsole.warn.apply(console, arguments)
-  }
-
-  window.onerror = function (msg, url, lineNo, columnNo, error) {
-    let data = {
-      message: '' + msg,
-    }
-
-    if (error.stack) {
-      data['stacktrace'] = error.stack
-    }
-
-    ctx.sendLog('error', data)
-
-    return false
   }
 })()
